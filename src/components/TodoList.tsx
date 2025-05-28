c7e1e4686ace3713eaf480a1008be439fe4c378d
Kouvousis <dexous1996@gmail.com>
2025-05-28 20:39:49 +0300

    feat: add toggle, commit task functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import {Edit, Save, Trash2, X} from "lucide-react";
+import {CheckSquare, Edit, Save, Square, Trash2, X} from "lucide-react";
 import type {TodoListProps} from "../types.ts";
 import {useState} from "react";
 
@@ -26,12 +26,18 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
         setEditText("");
     }
 
+    const handleToggle = (id: number) => () => {
+        dispatch({type: "COMPLETED", payload: id})
+    }
+
     return (
         <>
             <ul className="space-y-2">
                 {todos.map(
                     todo => (
-                        <li key={todo.id} className="flex items-center justify-between bg-gray-100 p-2 rounded ">
+                        <li key={todo.id}
+                            className="flex items-center justify-betweenbg-gray-100 p-2 rounded"
+                        >
                             {editId === todo.id ? (
                                 <>
                                     <div className="flex felx-1 gap-2">
@@ -58,7 +64,22 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
                                 </>
                             ) : (
                                 <>
-                                    <span>{todo.text}</span>
+                                    <div className="flex item-center gap-2 flex-1">
+                                        <button
+                                            className="text-green-500"
+                                            onClick={handleToggle(todo.id)}
+                                        >
+                                            {todo.completed ? (
+                                                <CheckSquare size={18}/>
+
+                                            ) : (
+                                                <Square size={18}/>
+
+                                            )}
+                                        </button>
+                                        <span>{todo.text}</span>
+                                    </div>
+
                                     <div className="flex gap-2">
                                         <button
                                             onClick={handleEdit(todo.id, todo.text)}
